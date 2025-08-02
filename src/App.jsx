f6fc274b59d33b9a6274d034f97766979d06a33d
Priyanshu Shishodia <priyanshushishodia008@gmail.com>
2025-08-03 02:36:42 +0530

    add scroll

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,23 @@ import Skills from "./components/Skills";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
+import SmoothScroll from "./components/SmoothScroller";
 import SocialLinks from "./components/SocialLinks";
 function App() {
- 
-
   return (
     <>
       <NavBar />
-      <SocialLinks/>
-      <Home />
-      <About />
-      <Skills />
-      <Portfolio />
-      
-      <Contact />
+      <SocialLinks />
+      <SmoothScroll>
+        <Home />
+        <About />
+        <Skills />
+        <Portfolio />
+
+        <Contact />
+      </SmoothScroll>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
